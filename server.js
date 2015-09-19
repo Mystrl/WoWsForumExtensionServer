10795fd60b93a741a4f777af12c81b7223a20233
JimQi <jim.qi101@gmail.com>
2015-09-18 18:42:06 -0700

    added recreateJSON function that restores the proper format for a single users data

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ function init() {
  * @param {function} callback2 - used to send result back to init();
  *
  */
- function getUserData(userid, callback2) {
- 	var result = '';
- 	var options = {
+function getUserData(userid, callback2) {
+	var result = '';
+	var options = {
  		host: 'api.worldofwarships.com',
  		path: '/wows/account/info/?application_id=demo&extra=statistics.pvp_solo&account_id=' + userid
  	};
@@ -62,14 +62,15 @@ function init() {
  	http.request(options, callback).end();
  }
 
- function storeData(userid, str) {
+function storeData(userid, str) {
  	var conString = "postgres://cfeijyxzuzivie:Uw7oiu8MRXIwP1P9Pv_pCnEarj@ec2-54-235-162-144.compute-1.amazonaws.com:5432/d2ertkkobk0u52?sslmode=require";
  	var client = new pg.Client(conString);
+ 	var formattedJSON = recreateJSON(str);
  	client.connect(function(err) {
  		if(err) {
  			return console.error('could not connect to postgres', err);
  		}
- 		client.query('INSERT INTO users VALUES (' + userid + ", '" + JSON.stringify(str) +"');", function(err, result) {
+ 		client.query('INSERT INTO users VALUES (' + userid + ", '" + formattedJSON +"');", function(err, result) {
  			if(err) {
  				return console.error('query failed', err);
  			}
@@ -79,4 +80,15 @@ function init() {
 
  }
 
- init();
+/*
+ * When we split the json response from the wargaming api into individual users we lose part of the structure. This functions adds it back in
+ *
+ * @param {json} str - string represent part of our json object. We'll wrap it with some stuff
+ *
+ */
+function recreateJSON(str) {
+	var json = '{"status":"ok","meta":{"count":1},"data":' + JSON.stringify(str) +'}}';
+	return json;
+}
+
+init();
