374211808867ffa6aad1350fe7310a863b51338d
JimQi <jim.qi101@gmail.com>
2015-09-18 17:48:39 -0700

    .split errors were caused by favicon requests resulting in an undefined user id. Added an if block to handle favicon requests

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ function init() {
 	const port = 8001;
 
 	var server = http.createServer(function(request, response) {
+
+		if (request.url === '/favicon.ico') {
+			//r.writeHead(200, {'Content-Type': 'image/x-icon'} );
+			response.end();
+			console.log('favicon requested');
+			return;
+		}
 		response.setHeader('Access-Control-Allow-Origin', 'chrome-extension://pmbkfeiiphpkcbenfodfeoclgbinpdmb');
 		response.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 		response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -14,7 +21,7 @@ function init() {
 		//userid is a comma delimited array
 		var userid = queryAsObject.userid;
 		var idArray = userid.split(",");
-		
+		console.log(idArray);
 		getUserData(userid, function(response2) {
 			//split the user data so we can store it as individual enteries
 			for (var i = 0; i < idArray.length; i++) {
@@ -35,43 +42,43 @@ function init() {
  * @param {function} callback2 - used to send result back to init();
  *
  */
-function getUserData(userid, callback2) {
-	var result = '';
-	var options = {
-		host: 'api.worldofwarships.com',
-		path: '/wows/account/info/?application_id=demo&extra=statistics.pvp_solo&account_id=' + userid
-	};
+ function getUserData(userid, callback2) {
+ 	var result = '';
+ 	var options = {
+ 		host: 'api.worldofwarships.com',
+ 		path: '/wows/account/info/?application_id=demo&extra=statistics.pvp_solo&account_id=' + userid
+ 	};
 
-	callback = function(response) {
-		var str ='';
+ 	callback = function(response) {
+ 		var str ='';
 
-		response.on('data', function (chunk) {
-			str += chunk;
-		});
+ 		response.on('data', function (chunk) {
+ 			str += chunk;
+ 		});
 
-		response.on('end', function() {
-			return callback2(str);
-		})
-	}
-	
-	http.request(options, callback).end();
-}
+ 		response.on('end', function() {
+ 			return callback2(str);
+ 		})
+ 	}
 
-function storeData(userid, str) {
-	var conString = "postgres://cfeijyxzuzivie:Uw7oiu8MRXIwP1P9Pv_pCnEarj@ec2-54-235-162-144.compute-1.amazonaws.com:5432/d2ertkkobk0u52";
-	var client = new pg.Client(conString);
-	client.connect(function(err) {
-		if(err) {
-			return console.error('could not connect to postgres', err);
-		}
-		client.query('INSERT INTO users VALUES (' + userid + ", '" + str +"');", function(err, result) {
-			if(err) {
-				return console.error('query failed', err);
-			}
-			client.end();
-		});
-	});
+ 	http.request(options, callback).end();
+ }
 
-}
+ function storeData(userid, str) {
+ 	var conString = "postgres://cfeijyxzuzivie:Uw7oiu8MRXIwP1P9Pv_pCnEarj@ec2-54-235-162-144.compute-1.amazonaws.com:5432/d2ertkkobk0u52?sslmode=require";
+ 	var client = new pg.Client(conString);
+ 	client.connect(function(err) {
+ 		if(err) {
+ 			return console.error('could not connect to postgres', err);
+ 		}
+ 		client.query('INSERT INTO users VALUES (' + userid + ", '" + str +"');", function(err, result) {
+ 			if(err) {
+ 				return console.error('query failed', err);
+ 			}
+ 			client.end();
+ 		});
+ 	});
 
-init();
+ }
+
+ init();
