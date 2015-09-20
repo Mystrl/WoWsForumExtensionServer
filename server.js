a193730d7e3304eb38e3a4451bbc6b7208a49341
JimQi <jim.qi101@gmail.com>
2015-09-20 00:26:34 -0700

    moved cache related functionality into their own functions. changed the way user data is stored.

--- server.js
+++ server.js
@@ -17,17 +17,61 @@ function init() {
 		//userid is a comma delimited array
 		var userid = queryAsObject.userid;
 		var idArray = userid.split(",");
-		getUserData(userid, function(response2) {
-			//split the user data so we can store it as individual enteries
-			for (var i = 0; i < idArray.length; i++) {
-				var accessID = idArray[i];
-				var json = JSON.parse(response2);
-				storeData(accessID, json.data[accessID]);	
-			}
-			response.end(response2);
-		});
+
+		getResponseData(idArray, response);
 	});
 	server.listen(process.env.PORT || port);
+}
+
+//TODO
+function getResponseData(userid, response) {
+	//split user ids into cached and notcached
+	//use getUserData to get jsons for ids not in cache
+	//user getFromCache for ids in cache
+	var useridInCache = [];
+	var useridNotCached = [];
+	for (var j = 0; j < userid.length; j++) {
+		if(!inCache(userid[j])) {
+			useridNotCached.push(userid[j]);
+			}
+		else {
+			useridInCache.push(userid[j]);
+		}
+	}
+
+	//remember to update count
+
+	//we build a responseJSON from cached and uncached data
+	var responseJSON = {"status":"ok","meta":{"count":0},"data":{}};
+
+	if (useridInCache.length > 0) {
+		for (var cachedUserIDIndex = 0; cachedUserIDIndex < useridInCache.length; cachedUserIDIndex++) {
+			var userid = useridInCache[cachedUserIDIndex]
+			var jsonFromCache = getFromCache(userid);
+			responseJSON.data[userid] = jsonFromCache;
+			//TODO implement getFromCache
+		}
+	}
+
+	//if there are uncached user ids we need to get them from the api and then add them to responseJSON before sending it to the client
+	if (useridNotCached.length > 0) {
+		getUserData(useridNotCached, function(jsonString) {;
+			//split the user data so we can store it as individual enteries
+			for (var unCachedUserIdIndex = 0; unCachedUserIdIndex < useridNotCached.length; unCachedUserIdIndex++) {
+				var accessID = useridNotCached[unCachedUserIdIndex];
+				var json = JSON.parse(jsonString);
+				storeData(accessID, json.data[accessID]);
+
+				responseJSON.data[accessID] = json.data[accessID];
+			}
+			response.end(JSON.stringify(responseJSON));
+		});
+	}
+	//otherwise just send responseJSON
+	else {
+		response.end(JSON.stringify(responseJSON));
+	}
+
 }
 
 /*
@@ -37,7 +81,7 @@ function init() {
  * @param {function} callback2 - used to send result back to init();
  *
  */
-function getUserData(userid, callback2) {
+function getUserData(userid, getResponseDataCallback) {
 	var result = '';
 	var options = {
  		host: 'api.worldofwarships.com',
@@ -52,13 +96,18 @@ function getUserData(userid, callback2) {
  		});
 
  		response.on('end', function() {
- 			callback2(str);
+ 			getResponseDataCallback(str);
  		})
  	}
 
  	http.request(options, callback).end();
  }
 
+//TODO
+// should deal with outdated information in here
+function inCache(userid) {
+	return false;
+}
 /*
  * Returns a json containing user data on all userid's in the userid array
  *
@@ -69,12 +118,12 @@ function getUserData(userid, callback2) {
 function storeData(userid, str) {
  	var conString = "postgres://cfeijyxzuzivie:Uw7oiu8MRXIwP1P9Pv_pCnEarj@ec2-54-235-162-144.compute-1.amazonaws.com:5432/d2ertkkobk0u52?sslmode=require";
  	var client = new pg.Client(conString);
- 	var formattedJSON = recreateJSON(str);
+ 	//var formattedJSON = recreateJSON(str);
  	client.connect(function(err) {
  		if(err) {
  			return console.error('could not connect to postgres', err);
  		}
- 		client.query('INSERT INTO users VALUES (' + userid + ", '" + formattedJSON +"');", function(err, result) {
+ 		client.query('INSERT INTO users VALUES (' + userid + ", '" + str +"');", function(err, result) {
  			if(err) {
  				return console.error('query failed', err);
  			}
